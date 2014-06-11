e8d28c7ffff4b0e67ee5982168ae2575108e0594
radko26 <radipirate@gmail.com>
2014-06-11 17:55:49 +0300

    content
    update

--- src/main/java/com/sirma/itt/javacourse/intro/bigint/Main.java
+++ src/main/java/com/sirma/itt/javacourse/intro/bigint/Main.java
@@ -1,7 +1,7 @@
-package main.java.com.sirma.itt.javacourse.BigInt.Main;
+package main.java.com.sirma.itt.javacourse.intro.bigint;
 
 /**
- * 	Own implementation of summing two big numbers
+ * Own implementation of summing two big numbers
  * 
  * @author Radoslav
  */
@@ -18,9 +18,18 @@ class Main {
 	 * Method for printing a char to the screen
 	 * 
 	 * @param string
-	 * 		the number in char array so that can be read digit by digit
+	 *            the number in char array so that can be read digit by digit
 	 * @param len
-	 * 		the preferred length of the printed characters
+	 *            the preferred length of the printed characters
+	 */
+
+	/**
+	 * 
+	 * @param string
+	 *            our string for printing
+	 * @param len
+	 *            the size of the given string the result is printing every char
+	 *            to the screen
 	 */
 	static void print(char[] string, int len) {
 		for (int i = 0; i < len; i++)
@@ -28,45 +37,51 @@ class Main {
 		System.out.println("");
 	}
 
+	/**
+	 * main method which do all the calculations
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		char[] first = new char[100000];
 		char[] second = new char[100000];
 		char[] sum = new char[100000];
 		int size = 10;
-		
+
 		for (int i = 0, value = 1; i < size; i++, value++) {
 			if (value == 10)
 				value = 0;
-			first[i] = (char) (value%7 + '0');
+			first[i] = (char) (value % 7 + '0');
 		}
 		for (int i = 0, value = 9; i < size; i++, value--) {
 			if (value == -1)
 				value = 10;
-			second[i] = (char) (value%8 + '0');
+			second[i] = (char) (value % 8 + '0');
 		}
-		
+
 		print(first, size);
 		print(second, size);
 
 		boolean above10 = false;
 		int value = 0;
-		for (int firstNumberIndex = size - 1, secondNumberIndex = size - 1, sumIndex = size; sumIndex >-1; firstNumberIndex--, secondNumberIndex--, sumIndex--) {
+		for (int firstNumberIndex = size - 1, secondNumberIndex = size - 1, sumIndex = size; sumIndex > -1; firstNumberIndex--, secondNumberIndex--, sumIndex--) {
 
 			if (above10 == true)
 				value = 1;
 			else
 				value = 0;
-			//if there are digits to be added from the second number
-			if (firstNumberIndex < 0 && secondNumberIndex>=0) {
+			// if there are digits to be added from the second number
+			if (firstNumberIndex < 0 && secondNumberIndex >= 0) {
 				value = value + (int) (second[secondNumberIndex] - '0');
 			}
-			// no digits left in the second but we add some from the first number
-			if (secondNumberIndex < 0 && firstNumberIndex>=0) {
+			// no digits left in the second but we add some from the first
+			// number
+			if (secondNumberIndex < 0 && firstNumberIndex >= 0) {
 				value = value + (int) (first[firstNumberIndex] - '0');
-			} 
-			//both numbers have digits
-			if(secondNumberIndex >=0 && firstNumberIndex >=0) {
+			}
+			// both numbers have digits
+			if (secondNumberIndex >= 0 && firstNumberIndex >= 0) {
 				value = value + (int) (first[firstNumberIndex] - '0')
 						+ (int) (second[secondNumberIndex] - '0');
 				if (value >= 10) {
@@ -77,15 +92,15 @@ class Main {
 			}
 			sum[sumIndex] = (char) (value + '0');
 		}
-		
+
 		System.out.print("Sum: ");
-		if (sum[0]-'0' !=0 ) {//nothing special, only checking if we need to add new digit to the sum
+		if (sum[0] - '0' != 0) {// nothing special, only checking if we need to
+								// add new digit to the sum
 			System.out.print(sum[0]);
 		}
 		for (int i = 1; i <= size; i++) {
-				System.out.print(sum[i]);
+			System.out.print(sum[i]);
 		}
-
 	}
 
 }
---
+++ src/main/java/com/sirma/itt/javacourse/intro/hangman/Main/Main.java
@@ -0,0 +1,118 @@
+package main.java.com.sirma.itt.javacourse.intro.hangman.Main;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import main.java.com.sirma.itt.javacourse.intro.hangman.WordGen.GenInfo;
+import main.java.com.sirma.itt.javacourse.intro.hangman.WordGen.Random;
+import main.java.com.sirma.itt.javacourse.intro.hangman.WordGen.RandomWordGenerator;
+
+/**
+ * 
+ * 
+ * @author Radoslav
+ */
+
+public class Main {
+
+	private static char guess;
+	private static Scanner input;
+	private static RandomWordGenerator wordGen;
+	private static int len;
+	private static int tries;
+	private static String word;
+	private static int correctGuesses;
+	private static boolean[] guessesResult;
+
+	/**
+	 * give all variables a initialization value
+	 */
+	private static void init() {
+		guess = 0;
+		tries = 10;
+		len = Random.getRand(4, 8);
+		correctGuesses = 0;
+		wordGen = new RandomWordGenerator(len);
+		wordGen.generate();
+		word = wordGen.getWord();
+		guessesResult = new boolean[word.length()];
+
+	}
+
+	/**
+	 * 
+	 * @param guess
+	 *            the char guess which was given
+	 * @return
+	 */
+	private static boolean checkGuess(char guess) {
+		boolean result = false;
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == guess) {
+				guessesResult[i] = true;
+				result = true;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * method for printing the given result
+	 */
+	private static void printHangman() {
+		for (int i = 0; i < word.length(); i++) {
+			if (guessesResult[i]) {
+				System.out.print(word.charAt(i));
+			} else {
+				System.out.print(" _ ");
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param args
+	 * @throws IOException
+	 *             input from the keyboard
+	 */
+	public static void main(String[] args) throws IOException {
+		init();
+
+		System.out.println("Your word contains: " + len + " symbols. ");
+		System.out.println(GenInfo.getBigLetterCount() + " UpperCase letters, "
+				+ GenInfo.getSmallLetterCount() + " LowerCase letters, "
+				+ GenInfo.getNumberCount() + " digits");
+		System.out.println("\nYou have " + tries + " tries to guess it.");
+		System.out.println("");
+
+		while (true) {
+			printHangman();
+			System.out.println("");
+
+			System.out.println("Insert your guess: ");
+
+			do {
+				guess = (char) System.in.read();
+			} while (guess == '\n' | guess == '\r');
+
+			if (!checkGuess(guess)) {
+				tries--;
+				System.out.print("Wrong guess \n");
+			} else {
+				correctGuesses++;
+				System.out.print("Yuppy " + (word.length() - correctGuesses)
+						+ " more \nF");
+			}
+			if (correctGuesses == word.length()) {
+				System.out.print("You win \n");
+				break;
+			}
+			if (tries == 0) {
+				System.out.print("You loose\n");
+				System.out.print("The word was " + word + "\n");
+				break;
+			}
+		}
+	}
+
+}
